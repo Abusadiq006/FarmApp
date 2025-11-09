5dfe48ccb4abc0613743cd90c420514a2313c472
Farmer Abusadiq <Abusadiq006@gmail.com>
2025-11-09 03:18:23 -0800

    Add images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import React from "react"
 import "../index.css"
+import bellpepper from "../assets/bellpepper.jpg"
+import cucumber from "../assets/cucumber.jpg"
+import habanero from "../assets/habanero.jpg"
+import sweetcorn from "../assets/sweetcorn.jpeg"
+import sweetpotatoes from "../assets/sweetpotatoes.jpg"
+import tomatoes from "../assets/tomatoes.jpg"
 
 
 const Home = () => {
@@ -15,12 +21,30 @@ const Home = () => {
             <div className="home fade-in">
                 <h1 className="home-title">Our Fresh Produce</h1>
                 <ul className="produce-list">
-                    <li> *SweetCorn* </li>
-                    <li> *Tomatoes* </li>
-                    <li> *Bell Pepper* </li>
-                    <li> *Hebanero* </li>
-                    <li> *Sweet Patatoes* </li>
-                    <li> *Maize* </li>
+                    <li> 
+                        <img src={sweetcorn} alt="Sweet Corn" />
+                        <span> *SweetCorn* </span> 
+                    </li>
+                    <li>
+                        <img src={tomatoes} alt="Tomatoes" />
+                        <span> *Tomatoes* </span>  
+                    </li>
+                    <li>
+                        <img src={bellpepper} alt="Bell Pepper" />
+                        <span> *Bell Pepper* </span> 
+                    </li>
+                    <li> 
+                        <img src={habanero} alt="habanero" />
+                        <span> *Habanero* </span> 
+                    </li>
+                    <li> 
+                        <img src={sweetpotatoes} alt="Sweet Potatoes" />
+                        <span> *Sweet Patatoes* </span> 
+                    </li>
+                    <li> 
+                        <img src={cucumber} alt="cucumber" />
+                        <span> *Cucumber* </span> 
+                    </li>
                 </ul>
             </div>
 
